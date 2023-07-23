e7be228b8a300d3c555797dbeccff2335f79f2d1
= <=>
2023-07-23 22:32:22 +0800

    refactor (app): refactor #6

--- app.py
+++ app.py
@@ -5,25 +5,18 @@ from src.utils.hackmd_note import HackmdNote
 from src.utils.url_processor import join_url
 
 
-if __name__ == "__main__":
-    PROGRAM_CSV_PATH = "note_data/WIP_ 2022 Pre-CFP empty schedule.csv"
-    PROGRAM_OUTPUT_CSV_PATH = "output/WIP_ 2022 Pre-CFP empty schedule.csv"
-    TEMPLATE_STORAGE_PATH = "note_template/pycon_apac_2022/"
+def execute_program_collabwriting_2022(hackmd_note: HackmdNote):
+    PROGRAM_CSV_PATH_2022 = "note_data/WIP_ 2022 Pre-CFP empty schedule.csv"
+    PROGRAM_OUTPUT_CSV_PATH_2022 = "output/WIP_ 2022 Pre-CFP empty schedule.csv"
+    TEMPLATE_STORAGE_PATH_2022 = "note_template/pycon_apac_2022/"
 
-    HACKMD_API_USER_API_ROUTE = ""
-    HACKMD_API_TEAMS_API_ROUTE = join_url(
-        base_url="teams", relative_url=settings.TEAM_PATH
-    )
-
-    hackmd_note = HackmdNote(hackmd_api_route=HACKMD_API_TEAMS_API_ROUTE)
-
-    program_info = raed_program_from_csv(read_file_path=PROGRAM_CSV_PATH)
+    program_info = raed_program_from_csv(read_file_path=PROGRAM_CSV_PATH_2022)
     program_info_title = program_info["notes_title"]
     program_info_content = program_info["notes_content"]
 
     program_collabwriting_2022 = ProgramCollabwriting(
         hackmd_note=hackmd_note,
-        template_storage_path=TEMPLATE_STORAGE_PATH,
+        template_storage_path=TEMPLATE_STORAGE_PATH_2022,
         notes_title=program_info_title,
         notes_content=program_info_content[:2],
     )
@@ -32,7 +25,18 @@ if __name__ == "__main__":
     program_collabwriting_2022.create_program_collabwriting_toc()
 
     write_program_to_csv(
-        output_file_path=PROGRAM_OUTPUT_CSV_PATH,
+        output_file_path=PROGRAM_OUTPUT_CSV_PATH_2022,
         notes_content=program_collabwriting_2022.get_notes_content(),
         notes_title=program_collabwriting_2022.get_notes_title(),
     )
+
+
+if __name__ == "__main__":
+    HACKMD_API_USER_API_ROUTE = ""
+    HACKMD_API_TEAMS_API_ROUTE = join_url(
+        base_url="teams", relative_url=settings.TEAM_PATH
+    )
+
+    hackmd_note = HackmdNote(hackmd_api_route=HACKMD_API_USER_API_ROUTE)
+
+    execute_program_collabwriting_2022(hackmd_note=hackmd_note)
