1f07418bb30b1e1a58533bcbf8b2608e4510f61d
= <=>
2023-07-23 15:03:10 +0800

    refactor (config): modify env setting #6

--- src/config/config.py
+++ src/config/config.py
@@ -4,11 +4,11 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     TEAM_PATH: str
     TOKEN: str
-    HACKMD_API_URL: str
+    HACKMD_API_URL: str = "https://api.hackmd.io/v1/"
 
-    NOTE_DATA_PATH = "note_data/"
-    OUTPUT_PATH = "output/"
-    LOG_PATH = "log/"
+    NOTE_DATA_PATH: str = "note_data/"
+    OUTPUT_PATH: str = "output/"
+    LOG_PATH: str = "log/"
 
     class Config:
         env_file = ".env"
